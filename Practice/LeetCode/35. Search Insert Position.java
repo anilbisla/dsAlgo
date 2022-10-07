fd3ef14b6344d84dabdacdaad278d8842e36c9cd
Kumar <Anil.Kumar2@transformco.com>
2022-10-07 11:17:35 +0530

    LeetCode practice

---
+++ Practice/LeetCode/35. Search Insert Position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+		return Solution.performBinarySearch(nums, 0, nums.length - 1, target);
+    }
+
+	private static int performBinarySearch(int[] nums, int start, int end, int target) {
+		int mid = start + (end - start) / 2;
+		if (start > end) {
+			return start;
+		}
+		if (nums[mid] == target) {
+			return mid;
+		} else if (nums[mid] > target) {
+			return performBinarySearch(nums, start, mid -1, target);
+		} else if (nums[mid] < target) {
+			return performBinarySearch(nums, mid + 1, end, target);
+		}
+		return -1;
+	}
+}
